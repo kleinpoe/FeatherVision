44847ed90ec7b440dec90acf5ac865b25574f37e
kleinpoe <kleinpoe@web.de>
2024-03-30 22:08:18 +0100

    adding detections to clip save result

--- Application/Video/ClipSaver.py
+++ Application/Video/ClipSaver.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 from logging import Logger
 
+from Surveillance.ObjectDetection.Detection import Detection
 from Surveillance.History.DetectionHistoryEntry import DetectionHistoryEntry
 from Config.Config import Config
 from Infrastructure.Clock import Clock
@@ -9,7 +10,11 @@ from Video.AnnotatedClipSaver import AnnotatedClipSaver
 from Video.HighResClipSaver import HighResClipSaver
 from Video.ThumbnailSaver import ThumbnailSaver
 
-
+    
+@dataclass
+class DetectionsInClip:
+    Time: timedelta
+    Detections: list[Detection]
 
 class ClipSaver:
     def __init__(self, 
@@ -32,11 +37,14 @@ class ClipSaver:
         HighResClipFilePath:str
         AnnotatedClipFilePath:str
         ClipDuration: timedelta
-        DateTime: datetime
+        Datetime: datetime
+        Detections: list[DetectionsInClip]
+    
         
     def Save(self, detectionHistoryEntries:list[DetectionHistoryEntry]) -> Result:
-        timestamp = self.clock.Now()
+        timestamp = detectionHistoryEntries[0].Timestamp
         annotatedPath = self.annotatedClipSaver.Save(timestamp, detectionHistoryEntries)
         highResResult = self.highResClipSaver.Save(timestamp,detectionHistoryEntries[0].Timestamp, detectionHistoryEntries[-1].Timestamp )
         thumbnailPath = self.thumbnailSaver.Save(timestamp, detectionHistoryEntries)
-        return ClipSaver.Result(thumbnailPath, highResResult.FilePath, annotatedPath, highResResult.Duration, timestamp)
+        detections = [DetectionsInClip(x.Timestamp-detectionHistoryEntries[0].Timestamp,x.Detections) for x in detectionHistoryEntries]
+        return ClipSaver.Result(thumbnailPath, highResResult.FilePath, annotatedPath, highResResult.Duration, timestamp,detections)
